04bd9d57d4f38e477296ef942082a9fe57f8b5a5
ybouhjira <b.youssef91@gmail.com>
2014-06-13 21:51:31 +0100

    Added klein.php (fixes #1)

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit25c34deaa56cf083e44503e51264e6e0::getLoader();
+return ComposerAutoloaderInit596b92f80ebaa22d11befa48200d2a05::getLoader();
