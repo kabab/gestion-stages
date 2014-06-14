52f952510c90923c8d3557f5a0cebfc4687f8c05
ybouhjira <b.youssef91@gmail.com>
2014-06-14 20:24:49 +0100

    Improved routing to static files

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-if (preg_match('/\.(?:png|jpg|jpeg|gif|js|css)$/', $_SERVER["REQUEST_URI"])) {
+if (preg_match('#^/web/#', $_SERVER["REQUEST_URI"])) {
     return false;    // serve the requested resource as-is.
 }
 
